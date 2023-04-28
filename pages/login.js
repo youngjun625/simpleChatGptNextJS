1a1341c0eca2f835f56d863a9a2215e2d6f39672
YJ at gram <youngjun625@hotmail.com>
2023-04-28 19:42:33 +0900

    Head test login

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,13 @@ const Login = () => {
     const [pw, setPw] = useState("")
 
     return <div>
+        <Head>
+          <title>Narrator</title>
+          <meta property="og:title" content={ `LOG IN` }></meta>
+            <meta property="og:type" content="website"></meta>
+            <meta property="og:image" content={'https://dotzkorea.s3.ap-northeast-2.amazonaws.com/narrator/asset/narrator_logo.png'}></meta>
+            <meta property="og:description" content={ 'LOG IN PAGE' }></meta>
+        </Head>
         <input type="text" value={email} onChange={e=> setEmail(e.target.value)}/>
         <input type="text" value={pw} onChange={e=> setEmail(e.target.value)}/>
     </div>
